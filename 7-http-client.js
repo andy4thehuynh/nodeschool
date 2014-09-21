008794ac2b7183c772863e429cbb415e72b59ac8
Andy Huynh <andy4thehuynh@gmail.com>
2014-09-20 22:26:00 -0700

    ALH: Refactor HTTP Client

--- 7-http-client.js
+++ 7-http-client.js
@@ -2,7 +2,21 @@ var http = require('http');
 var url = process.argv[2];
 
 http.get(url, function(response) {
+  response.setEncoding('utf8');
   response.on('data', function(data) {
-    console.log(data.toString());
+    console.log(data);
+  });
+  response.on('error', function(error) {
+    console.error(error);
   });
 });
+
+
+// NODESCHOOL SOLUTION:
+// var http = require('http');
+//
+// http.get(process.argv[2], function(response) {
+//   response.setEncoding('utf8');
+//   response.on('data', console.log);
+//   response.on('error', console.error);
+// });
